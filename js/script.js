5bb5baba79f9521f8eaf25f9a3bb19e16f9a8517
Hayel Al-Faisali <97064734+Hayelalfaisali@users.noreply.github.com>
2022-12-26 03:19:54 +0300

    Done

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+let social = document.querySelector('.social_media');
+let share = document.querySelector('.share');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+    header.style.visibility="visible";
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+social.onclick = () =>{
+
+    share.style.visibility="visible";
+    
+}
